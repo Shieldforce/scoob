e7f96d52585713784666d7d7be8c27c58e98101e
Alexandre Ferreira <shieldforce2@gmail.com>
2022-12-26 23:29:58 -0300

    implementação do help

--- progs/help.sh
+++ progs/help.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "Help!"
+echo -e "\e[33;36m |\e[33;31m --------------------------- HELP! ---------------------------- \e[0m\e[33;36m|\e[0m";
+echo "";
+
+
+echo -e "\e[33;36m - Argumentos para docker : \e[0m";
+echo -e "\e[33;35m --type : Recebe o tipo de execução do (SCOOB) \e[0m";
 
