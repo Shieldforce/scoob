97e54bf792e5ba41e54968373ebe1620f1f3fcfd
Alexandre Ferreira <shieldforce2@gmail.com>
2024-12-27 16:14:05 -0300

    adicionando docker remove

--- progs/docker-remove.sh
+++ progs/docker-remove.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -v $2 ]; then
+if [ -v $3 ]; then
   bash ${path_dir}/progs/error.sh  "Você precisa passar um nome de container!";
   exit;
 fi
 
-if docker ps -a | grep "$2" &> /dev/null; then
-  docker stop "$2" && docker rm "$2";
-  docker image rm "$2" --force;
+if docker ps -a | grep "$3" &> /dev/null; then
+  docker stop "$3" && docker rm "$3";
+  docker image rm "$3" --force;
   echo ''
   bash ${path_dir}/progs/success.sh 'Container excluído com sucesso!'
 fi
