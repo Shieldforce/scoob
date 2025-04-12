a6cc3f8a5d0d37934a6be97c8a23ea1d6e40e03e
Alexandre Ferreira <shieldforce2@gmail.com>
2025-04-12 12:27:42 -0300

    scoob start

---
+++ progs/scoob-start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html composer/composer \
+     composer install --ignore-platform-reqs
+
