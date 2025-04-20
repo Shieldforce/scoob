edc6d11c4509d88a32b549d2fbe4399748fe8543
Alexandre Ferreira <shieldforce2@gmail.com>
2025-04-20 18:47:43 -0300

    filanização de container

--- progs/ini.sql
+++ progs/ini.sql
@@ -1,2 +1,13 @@
 CREATE USER IF NOT EXISTS 'scoob_user'@'%' IDENTIFIED BY 'scoob_pass';
 GRANT ALL PRIVILEGES ON scoob_db.* TO 'scoob_user'@'%' WITH GRANT OPTION;
+
+CREATE USER IF NOT EXISTS 'scoob_user'@'%.%.%.%' IDENTIFIED BY 'scoob_pass';
+GRANT ALL PRIVILEGES ON scoob_db.* TO 'scoob_user'@'%.%.%.%' WITH GRANT OPTION;
+
+CREATE USER IF NOT EXISTS 'scoob_user'@'0.0.0.0' IDENTIFIED BY 'scoob_pass';
+GRANT ALL PRIVILEGES ON scoob_db.* TO 'scoob_user'@'0.0.0.0' WITH GRANT OPTION;
+
+CREATE USER IF NOT EXISTS 'scoob_user'@'localhost' IDENTIFIED BY 'scoob_pass';
+GRANT ALL PRIVILEGES ON scoob_db.* TO 'scoob_user'@'localhost' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
