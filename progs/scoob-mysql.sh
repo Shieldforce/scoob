85080dd245e3fba81f9e5a6ac7dae7ed7df35f11
Alexandre Ferreira <shieldforce2@gmail.com>
2025-04-20 20:51:52 -0300

    mysql variáveis

--- progs/scoob-mysql.sh
+++ progs/scoob-mysql.sh
@@ -82,31 +82,31 @@ fi
 cat > "$init_sql_path" <<EOF
 -- User root
 
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${mysql_pass}';
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '$mysql_pass';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
-CREATE USER IF NOT EXISTS 'root'@'%.%.%.%' IDENTIFIED BY '${mysql_pass}';
+CREATE USER IF NOT EXISTS 'root'@'%.%.%.%' IDENTIFIED BY '$mysql_pass';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%.%.%.%' WITH GRANT OPTION;
 
-CREATE USER IF NOT EXISTS 'root'@'0.0.0.0' IDENTIFIED BY '${mysql_pass}';
+CREATE USER IF NOT EXISTS 'root'@'0.0.0.0' IDENTIFIED BY '$mysql_pass';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'0.0.0.0' WITH GRANT OPTION;
 
-CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '${mysql_pass}';
+CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '$mysql_pass';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 
 -- User criado
 
-CREATE USER IF NOT EXISTS '${mysql_user}'@'%' IDENTIFIED BY '${mysql_pass}';
-GRANT ALL PRIVILEGES ON ${mysql_db}.* TO '${mysql_user}'@'%' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS '$mysql_user'@'%' IDENTIFIED BY '$mysql_pass';
+GRANT ALL PRIVILEGES ON $mysql_db.* TO '$mysql_user'@'%' WITH GRANT OPTION;
 
-CREATE USER IF NOT EXISTS '${mysql_user}'@'%.%.%.%' IDENTIFIED BY '${mysql_pass}';
-GRANT ALL PRIVILEGES ON ${mysql_db}.* TO '${mysql_user}'@'%.%.%.%' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS '$mysql_user'@'%.%.%.%' IDENTIFIED BY '$mysql_pass';
+GRANT ALL PRIVILEGES ON $mysql_db.* TO '$mysql_user'@'%.%.%.%' WITH GRANT OPTION;
 
-CREATE USER IF NOT EXISTS '${mysql_user}'@'0.0.0.0' IDENTIFIED BY '${mysql_pass}';
-GRANT ALL PRIVILEGES ON ${mysql_db}.* TO '${mysql_user}'@'0.0.0.0' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS '$mysql_user'@'0.0.0.0' IDENTIFIED BY '$mysql_pass';
+GRANT ALL PRIVILEGES ON $mysql_db.* TO '$mysql_user'@'0.0.0.0' WITH GRANT OPTION;
 
-CREATE USER IF NOT EXISTS '${mysql_user}'@'localhost' IDENTIFIED BY '${mysql_pass}';
-GRANT ALL PRIVILEGES ON ${mysql_db}.* TO '${mysql_user}'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS '$mysql_user'@'localhost' IDENTIFIED BY '$mysql_pass';
+GRANT ALL PRIVILEGES ON $mysql_db.* TO '$mysql_user'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
 EOF
