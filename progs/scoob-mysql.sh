9ad72ec4776d408f18d772b7bb1977aa2cef4860
Alexandre Ferreira <shieldforce2@gmail.com>
2025-04-20 20:27:23 -0300

    variaveis mysql

--- progs/scoob-mysql.sh
+++ progs/scoob-mysql.sh
@@ -96,17 +96,17 @@ GRANT ALL PRIVILEGES ON *.* TO 'scoob_user'@'localhost' WITH GRANT OPTION;
 
 -- User default
 
-CREATE USER IF NOT EXISTS 'scoob_user'@'%' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON scoob_db.* TO 'scoob_user'@'%' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS '${$mysql_user}'@'%' IDENTIFIED BY '${db_pass}';
+GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO 'scoob_user'@'%' WITH GRANT OPTION;
 
-CREATE USER IF NOT EXISTS 'scoob_user'@'%.%.%.%' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON scoob_db.* TO 'scoob_user'@'%.%.%.%' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS '${$mysql_user}'@'%.%.%.%' IDENTIFIED BY '${db_pass}';
+GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO 'scoob_user'@'%.%.%.%' WITH GRANT OPTION;
 
-CREATE USER IF NOT EXISTS 'scoob_user'@'0.0.0.0' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON scoob_db.* TO 'scoob_user'@'0.0.0.0' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS '${$mysql_user}'@'0.0.0.0' IDENTIFIED BY '${db_pass}';
+GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO 'scoob_user'@'0.0.0.0' WITH GRANT OPTION;
 
-CREATE USER IF NOT EXISTS 'scoob_user'@'localhost' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON scoob_db.* TO 'scoob_user'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS '${$mysql_user}'@'localhost' IDENTIFIED BY '${db_pass}';
+GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO 'scoob_user'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
 EOF
