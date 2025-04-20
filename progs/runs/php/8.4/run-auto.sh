caf0d56c9dc2a077f6e75f78542a9d391a911b73
Alexandre Ferreira <shieldforce2@gmail.com>
2025-04-20 16:16:31 -0300

    criando diretorio scoob implements

--- progs/runs/php/8.4/run-auto.sh
+++ progs/runs/php/8.4/run-auto.sh
@@ -18,7 +18,7 @@ else
   if [ -d docker_scoob ]; then
     echo "Diretório scoob_implements ok!"
   else
-    ./progs/exec_spinner.sh \
+    bash ${path_dir}/progs/exec_spinner.sh \
         "mkdir scoob_implements" \
         "Criando diretório scoob_implements..."
   fi
