39816b2fa77469b702bd122be07f8df7063a26ae
Alexandre Ferreira <shieldforce2@gmail.com>
2025-04-20 20:30:47 -0300

    mysql variáveis

--- progs/scoob-mysql.sh
+++ progs/scoob-mysql.sh
@@ -83,30 +83,30 @@ cat > "$init_sql_path" <<EOF
 -- User root
 
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON *.* TO 'scoob_user'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
 CREATE USER IF NOT EXISTS 'root'@'%.%.%.%' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON *.* TO 'scoob_user'@'%.%.%.%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%.%.%.%' WITH GRANT OPTION;
 
 CREATE USER IF NOT EXISTS 'root'@'0.0.0.0' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON *.* TO 'scoob_user'@'0.0.0.0' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'0.0.0.0' WITH GRANT OPTION;
 
 CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON *.* TO 'scoob_user'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 
 -- User default
 
 CREATE USER IF NOT EXISTS '${$mysql_user}'@'%' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO 'scoob_user'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO '${$mysql_user}'@'%' WITH GRANT OPTION;
 
 CREATE USER IF NOT EXISTS '${$mysql_user}'@'%.%.%.%' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO 'scoob_user'@'%.%.%.%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO '${$mysql_user}'@'%.%.%.%' WITH GRANT OPTION;
 
 CREATE USER IF NOT EXISTS '${$mysql_user}'@'0.0.0.0' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO 'scoob_user'@'0.0.0.0' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO '${$mysql_user}'@'0.0.0.0' WITH GRANT OPTION;
 
 CREATE USER IF NOT EXISTS '${$mysql_user}'@'localhost' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO 'scoob_user'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${$mysql_db}.* TO '${$mysql_user}'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
 EOF
